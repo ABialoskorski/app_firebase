f36171a58ee1935b793229eb431856bcba7812d1
Artur Białoskórski <Artur@MacBook-Air-Klaudia.local>
2018-11-13 19:31:15 +0100

    Initial com

---
+++ app/src/main/java/com/example/artur/app/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.artur.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+        userRegistration = (Button) findViewById(R.id.btnRegister);
+
+
+        Info.setText("Number of attempts remaining: 3");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        /*
+        FirebaseAuth user = firebaseAuth.getCurrentUser();
+         */
+
+        if (user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, com.example.artur.app.RegistrationActivity.class));
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/artur/app/RegistrationActivity.java
@@ -0,0 +1,98 @@
+package com.example.artur.app;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.artur.app.MainActivity;
+import com.example.artur.app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText UserName;
+    private EditText UserPassword;
+    private EditText UserEmail;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        setupUIViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate()){
+
+                    //Upload data to the database//
+
+                    String user_email = UserEmail.getText().toString().trim();
+                    String user_password = UserPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+       /* Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent(RegistrationActivity.this,MainActivity.class);
+                startActivity(j);
+            }
+        }); */
+    }
+    private void setupUIViews() {
+        UserName = (EditText) findViewById(R.id.etUserName);
+        UserEmail = (EditText) findViewById(R.id.etUserEmail);
+        UserPassword = (EditText) findViewById(R.id.etUserPassword);
+        regButton = (Button)findViewById(R.id.btnRegister);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+
+        String name = UserName.getText().toString();
+        String password =  UserPassword.getText().toString();
+        String email = UserEmail.getText().toString();
+
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "Please enter lacking details", Toast.LENGTH_SHORT).show();
+        }else{
+            result = true;
+        }
+        return  result;
+
+
+    }
+
+}
