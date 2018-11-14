0382eb11d67b2651d8bf0f19176591e58089731d
Artur Białoskórski <Artur@MacBook-Air-Klaudia.local>
2018-11-14 19:45:06 +0100

    logout

--- app/src/main/java/com/example/artur/app/MainActivity.java
+++ app/src/main/java/com/example/artur/app/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
 
-        if (user == null){
+        if (user != null){
             finish();
             startActivity(new Intent(MainActivity.this, SecondActivity.class));
         }
--- app/src/main/java/com/example/artur/app/SecondActivity.java
+++ app/src/main/java/com/example/artur/app/SecondActivity.java
@@ -1,16 +1,48 @@
 package com.example.artur.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SecondActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
+    }
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
